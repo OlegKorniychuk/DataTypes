99baab5e601ca7a82dd7b39ce1ffe77ef63d6f44
Oleg Korniichuk <kornoleg576@gmail.com>
2021-09-28 21:35:46 +0300

    Last one was tough

--- Exercises/4-count-types.js
+++ Exercises/4-count-types.js
@@ -1,7 +1,15 @@
 'use strict';
 
-const arr = [1, 'jssucks', false, true, 'car', 1918, 25, 'memes', false]
-const result = ({ number: 0, string: 0, boolean: 0 })
-const countTypesInArray = null;
+const arr = [1, 'jssucks', false, true, 'car', 1918, 25, 'memes', false];
+const countTypesInArray = (array) => {
+  const vocabulary = ({});
+  for (const data of array) {
+    const type = typeof data;
+    vocabulary[type] ? vocabulary[type]++ : vocabulary[type] = 1;
+  }
+  return vocabulary;
+};
+
+console.log(countTypesInArray(arr));
 
 module.exports = { countTypesInArray };
